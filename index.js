821c685f764f6f8e61358993546aab7b987e8fb2
Rakshatnaik <60248567+Rakshatnaik@users.noreply.github.com>
2022-12-07 15:48:54 +0530

    reconstructed the password list

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e",
+    "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+    "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!",
+    "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",",
+    "|", ":", ";", "<", ">", ".", "?", "/"];
 let passcode1 = ""
 let passcode2 = ""
 
@@ -8,7 +12,7 @@ let password2 = document.getElementById("password2")
 
 
 function generatePassword() {
-    for(let i = 0; i < 16; i++) {
+    for (let i = 0; i < 16; i++) {
         let random1 = Math.floor(Math.random() * characters.length) + 1
         let random2 = Math.floor(Math.random() * characters.length) + 1
         passcode1 += characters[random1]
